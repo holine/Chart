ba646e23756fdaa12a39a9ee10733a451e4100be
lijiangang <lijiangang@dratio.com>
2013-08-23 17:31:57 +0800

    完成饼图的样式

--- chart.js
+++ chart.js
@@ -4,8 +4,8 @@ function Chart(cfg) {
 Chart.prototype.object = {
 	map : {},
 	area : {},
-	column:{},
-	pie:{}
+	column : {},
+	pie : {}
 };
 Chart.prototype.map = function(cfg) {
 	if (typeof (this.object.map[cfg.id]) == 'undefined') {
@@ -44,7 +44,49 @@ Chart.prototype.pie = function(cfg) {
 				typeof (cfg.width) == 'undefined' ? '100%' : cfg.width,
 				typeof (cfg.height) == 'undefined' ? '100%' : cfg.height, 0, 1);
 	}
+	cfg.data['styles'] = {
+		"definition" : [ {
+			"name" : "myLabelsFont",
+			"type" : "font",
+			"font" : "Arial",
+			"size" : "14",
+			"bold" : "1",
+			'borderColor' : '666666',
+			'bgColor' : 'EEEEEE',
+		} ],
+		"application" : [ {
+			"toobject" : "DataLabels",
+			"styles" : "myLabelsFont"
+		} ]
+	};
+	var chart = {
+		bgAlpha : 0,// flash背景透明度
+		showBorder : 0,// 是否显示flash边框
+		useRoundEdges : '1',
+		showPercentInToolTip : '1',
+		showPercentValues : '1',
+		shownames : '0',
+		animation : '1',
+		legendBgAlpha : '0',
+		legendShadow : '0',
+		legendBorderColor : 'eeeeee',
+		legendBorderAlpha : 0, // 边框透明度
+		baseFontSize : '12',
+		showlegend : '1',
+		legendPosition : 'RIGHT',
+		legendIconScale : '2',
+		enableSmartLabels : '0',
+		labelDisplay : 'WRAP',
+		showToolTip : 0,
+		showToolTipShadow : 1,
+		radius3D : 0,
+	// use3DLighting:0
+	};
+	for(var i in chart){
+		cfg.data['chart'][i] = chart[i];
+	}
 	this.object.pie[cfg.id].setJSONData(cfg.data);
+	this.object.pie[cfg.id].setTransparent(true);
 	this.object.pie[cfg.id].render(cfg.id);
 
 };
