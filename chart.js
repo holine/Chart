47784f7e8887e5bbd69fc51b0e1e87ceb0ecadcf
lijiangang <lijiangang@dratio.com>
2013-08-26 11:14:06 +0800

    添加标签云

--- chart.js
+++ chart.js
@@ -5,7 +5,8 @@ Chart.prototype.object = {
 	map : {},
 	area : {},
 	column : {},
-	pie : {}
+	pie : {},
+	tagCloud : {}
 };
 Chart.prototype.map = function(cfg) {
 	if (typeof (this.object.map[cfg.id]) == 'undefined') {
@@ -34,6 +35,10 @@ Chart.prototype.column = function(cfg) {
 				typeof (cfg.height) == 'undefined' ? '100%' : cfg.height, 0, 1);
 	}
 	var chart = {
+		formatNumber : 0,
+		formatNumberScale : 0,
+		placeValuesInside : 0,
+		lineColor : '#FFFFFF',
 		is2D : 0,
 		canvasBgAlpha : 0,
 		bgAlpha : 0,// flash背景透明度
@@ -56,10 +61,33 @@ Chart.prototype.column = function(cfg) {
 		showToolTip : 0,
 		showToolTipShadow : 1,
 		radius3D : 0,
+		xAxisName : '',
+		yAxisName : '',
+		caption : '',
+		numberPrefix : ''
 	// use3DLighting:0
 	};
 	for ( var i in chart) {
 		cfg.data['chart'][i] = chart[i];
+	}
+
+	cfg.data['styles'] = {
+		"definition" : [ {
+			"name" : "myValuesFont",
+			"type" : "font",
+			"size" : "12",
+			"color" : "FFFFFF",
+			"bold" : "1",
+			"bgcolor" : "666666",
+			"bordercolor" : "666666"
+		} ],
+		"application" : [ {
+			"toobject" : "DataValues",
+			"styles" : "myValuesFont"
+		} ]
+	};
+	for ( var i in cfg.data.data) {
+		cfg.data.data[i]['color'] = '#2985c8';// 柱状颜色
 	}
 	this.object.column[cfg.id].setJSONData(cfg.data);
 	this.object.column[cfg.id].setTransparent(true);
@@ -82,10 +110,16 @@ Chart.prototype.pie = function(cfg) {
 			"bold" : "1",
 			'borderColor' : '666666',
 			'bgColor' : 'EEEEEE',
+		}, {
+			"name" : "myLabelsAlpha",
+			"type" : "Animation",
+			"param" : "_alpha",
+			"start" : "0",
+			"duration" : "1",
 		} ],
 		"application" : [ {
 			"toobject" : "DataLabels",
-			"styles" : "myLabelsFont"
+			"styles" : "myLabelsAlpha,myLabelsFont"
 		} ]
 	};
 	var chart = {
@@ -117,5 +151,28 @@ Chart.prototype.pie = function(cfg) {
 	this.object.pie[cfg.id].setJSONData(cfg.data);
 	this.object.pie[cfg.id].setTransparent(true);
 	this.object.pie[cfg.id].render(cfg.id);
-
+};
+Chart.prototype.TagCloud = function(cfg) {
+	if (typeof (this.object.pie[cfg.id]) == 'undefined') {
+		this.object.tagCloud[cfg.id] = new SWFObject(this.path
+				+ '/assets/TagCloud.swf', cfg.id + '_chart',
+				typeof (cfg.width) == 'undefined' ? '100%' : cfg.width,
+				typeof (cfg.height) == 'undefined' ? '100%' : cfg.height, 9,
+				"#ffffff");
+	}
+	this.object.tagCloud[cfg.id].addParam("wmode", "transparent");
+	this.object.tagCloud[cfg.id].addParam("allowScriptAccess", "always");
+	this.object.tagCloud[cfg.id].addVariable("tcolor", "0x333333");
+	this.object.tagCloud[cfg.id].addVariable("tcolor2", "0x333333");
+	this.object.tagCloud[cfg.id].addVariable("hicolor", "0x000000");
+	this.object.tagCloud[cfg.id].addVariable("tspeed", "100");
+	this.object.tagCloud[cfg.id].addVariable("distr", "true");
+	this.object.tagCloud[cfg.id].addVariable("mode", "both");
+	var tags = '<tags>';
+	for ( var i in cfg.data.data) {
+		tags += '<a href="javascript:void(0)" class="tag-link-191" title="3 topics" style="font-size: 9.0243902439pt;" >' + cfg.data.data[i] + '</a>';
+	}
+	tags += '</tags>';
+	this.object.tagCloud[cfg.id].addVariable("tagcloud", encodeURIComponent(tags));
+	this.object.tagCloud[cfg.id].write(cfg.id);
 };
